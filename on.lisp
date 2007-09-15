bcbef1a8879257bba36b60bade42ba6843089db5
smithzv <smithzv@belial.local>
2007-09-15 00:26:47 -0600

    Added cut/cute, nondeterminism, and the ATN implementation to on.lisp

--- on.lisp
+++ on.lisp
@@ -256,30 +256,6 @@
 
 |#
 
-(defmacro partial-eval (fn &rest args)
-  (labels ((unk (sym) (if (eql sym '?) t nil))
-           (get-gensyms (lst)
-             (cond ((null lst) nil)
-                   (t (cons (gensym "PARTIAL-EVAL-")
-                            (get-gensyms (cdr lst)) ))))
-           (replace-unk (args gensyms)
-             (cond ((null args) nil)
-                   ((unk (car args)) (cons (car gensyms) (replace-unk (cdr args)
-                                                                      (cdr gensyms) )))
-                   (t (cons (car args) (replace-unk (cdr args) gensyms))) )))
-    (let ((gensyms (get-gensyms (remove-if-not #'unk args))))
-      `(lambda ,gensyms (funcall #',fn ,@(replace-unk args gensyms))) )))
-
-#| Examples
-
-(macroexpand-1
-'(partial-eval list a b ?)
-)
-
-(funcall (partial-eval + 1 pi 43 ?) 2)
-
-|#
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Function returning functions ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -311,6 +287,7 @@
         (funcall then x)
         (if else (funcall else x)) )))
 
+;;; Function intersection
 (defun fint (fn &rest fns)
   (if (null fns)
     fn
@@ -318,12 +295,26 @@
       #'(lambda (x)
           (and (funcall fn x) (funcall chain x)) ))))
 
+;;; Function union
 (defun fun (fn &rest fns)
   (if (null fns)
-    fn
-    (let ((chain (apply #'fun fns)))
-      #'(lambda (x)
-          (or (funcall fn x) (funcall chain x)) ))))
+      fn
+      (let ((chain (apply #'fun fns)))
+        #'(lambda (x)
+            (or (funcall fn x) (funcall chain x)) ))))
+
+#| Examples
+
+; Move up to the next even integer
+(funcall (fif #'oddp #'1+ #'identity) 4)
+
+; Integers which are odd and even (i.e. non of them)
+(mapcar (fint #'oddp #'evenp) '(1 2 3 4))
+
+; Integers which are odd or even (i.e. all of them)
+(mapcar (fun #'oddp #'evenp) '(1 2 3 4))
+
+|#
 
 (defun lrec (rec &optional base)
   (labels ((self (lst)
@@ -335,6 +326,28 @@
                         #'(lambda ()
                             (self (cdr lst)) )))))
     #'self ))
+
+#| Examples
+
+;length
+(lrec #'(lambda (x f) (1+ (funcall f))) 0)
+
+;every (for oddp)
+(lrec #'(lambda (x f) (and (oddp x) (funcall f))) t)
+
+;copy-list
+(lrec #'(lambda (x f) (cons x (funcall f))))
+
+;remove-duplicates
+(lrec #'(lambda (x f) (adjoin x (funcall f))))
+
+;find-if, for some function fn
+(lrec #'(lambda (x f) (if (fn x) x (funcall f))))
+
+;some, for some function fn
+(lrec #'(lambda (x f) (or (fn x) (funcall f))))
+
+|#
 
 (defun ttrav (rec &optional (base #'identity))
   (labels ((self (tree)
@@ -362,35 +375,60 @@
                         (self (cdr tree)) ))))))
     #'self ))
 
+#| Examples
+
+;copy-tree
+(ttrav #'cons)
+
+;count-leaves
+(ttrav #'(lambda (l r) (+ l (or r l))) 1)
+
+;flatten
+(ttrav #'nconc #'mklist)
+
+(funcall * '(1 2 3 (4 5)))
+
+;flatten
+(trec #'(lambda (o l r) (or (funcall l) (funcall r)))
+      #'(lambda (tree) (and (oddp tree) tree)) )
+
+;rfind-if for oddp
+(trec #'(lambda (o l r) (or (funcall l) (funcall r)))
+      #'(lambda (tree) (and (oddp tree) tree)) )
+
+|#
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Macros returning functions ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun rbuild (expr)
-  (if (or (atom expr) (eq (car expr) 'lambda))
-      expr
-      (if (eq (car expr) 'compose)
-          (build-compose (cdr expr))
-          (build-call (car expr) (cdr expr)) )))
+(with-compilation-unit (:override nil)
 
-(defun build-call (op fns)
-  (let ((g (gensym "BUILD-CALL-")))
-    `(lambda (,g)
-       (,op ,@(mapcar #'(lambda (f)
-                          `(,(rbuild f) ,g) )
-                      fns )))))
+  (defmacro fn (expr) `#',(rbuild expr))
 
-(defun build-compose (fns)
-  (let ((g (gensym "BUILD-COMPOSE-")))
-    `(lambda (,g)
-       ,(labels ((rec (fns)
-                   (if fns
-                       `(,(rbuild (car fns))
-                          ,(rec (cdr fns)) )
-                       g )))
-          (rec fns) ))))
+  (defun rbuild (expr)
+    (if (or (atom expr) (eq (car expr) 'lambda))
+        expr
+        (if (eq (car expr) 'compose)
+            (build-compose (cdr expr))
+            (build-call (car expr) (cdr expr)) )))
 
-(defmacro fn (expr) `#',(rbuild expr))
+  (defun build-call (op fns)
+    (let ((g (gensym "BUILD-CALL-")))
+      `(lambda (,g)
+         (,op ,@(mapcar #'(lambda (f)
+                            `(,(rbuild f) ,g) )
+                        fns )))))
+
+  (defun build-compose (fns)
+    (let ((g (gensym "BUILD-COMPOSE-")))
+      `(lambda (,g)
+         ,(labels ((rec (fns)
+                     (if fns
+                         `(,(rbuild (car fns))
+                            ,(rec (cdr fns)) )
+                         g )))
+            (rec fns) )))))
 
 (defmacro alrec (rec &optional base)
   (let ((gfn (gensym "ALREC-")))
@@ -412,6 +450,71 @@
 
 (defmacro on-trees (rec base &rest trees)
   `(funcall (atrec ,rec ,base) ,@trees) )
+
+(defmacro cute ((fn &rest args))
+  (labels ((gensym? (sym) (and (symbolp sym) (not (symbol-package sym))))
+           (get-gensyms (n) (cond ((= n 0) nil)
+                                  (t (cons (gensym "CUTE-") (get-gensyms (1- n)))) ))
+           (replace-slots (args)
+             (cond ((null args) nil)
+                   ((eql (car args) '<>)
+                    (cons (gensym "CUTE-") (replace-slots (cdr args))) )
+                   (t (cons (car args) (replace-slots (cdr args)))) )))
+    (let ((arglist (replace-slots args)))
+      `(let ,(mapcar #'list (get-gensyms (length arglist))
+                            (remove-if #'gensym? arglist) )
+         (lambda ,(remove-if-not #'gensym? arglist)
+           (funcall #',fn ,@arglist) )))))
+
+(defmacro cut ((fn &rest args))
+  (labels ((slot? (sym) (eq sym '<>))
+           (get-new-args (args)
+             (cond ((null args) nil)
+                   ((slot? (car args)) (cons (gensym "CUT-")
+                                             (get-new-args (cdr args)) ))
+                   (t (get-new-args (cdr args))) ))
+           (replace-slots (args gensyms)
+             (cond ((null args) nil)
+                   ((eql (car args) '<>)
+                    (cons (car gensyms) (replace-slots (cdr args) (cdr gensyms))) )
+                   (t (cons (car args) (replace-slots (cdr args) gensyms))) )))
+    (let* ((new-args (get-new-args args))
+           (arglist (replace-slots args new-args)) )
+      `(lambda ,new-args (funcall #',fn ,@arglist) ))))
+
+(defun split-on (fn lst)
+  (unless (null lst)
+    (multiple-value-bind (on off) (split-on fn (cdr lst))
+      (if (funcall fn (car lst))
+          (values (cons (car lst) on) off)
+          (values on (cons (car lst) off)) ))))
+
+(defmacro cut2 ((fn &rest args))
+  (labels ((slot? (sym) (eq sym '<>)))
+    (multiple-value-bind (slots vals)
+                         (split-on #'slot? args)
+      (list slots vals) )))
+
+(split-on #'oddp '(1 2 3 4 5))
+
+#| Examples
+
+(macroexpand-1
+  '(cut (list <> 1 (+ 1 1))) )
+
+(macroexpand-1
+  `(cut2 (list <> 1 2 3 <>)) )
+
+(let ((a 5))
+  (mapcar (cut (list a <> 1)) '(1 2 3 4)) )
+
+(mapcar (cut (* <> (+ 1 1))) '(1 2 3 4))
+
+(mapcar (compose (cut (cons 'hello <>)) #'list) '(how are you))
+
+(funcall #.(cut (+ 1 pi 43 <>)) 2)
+
+|#
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Lazy evaluation ;;;;
@@ -435,7 +538,10 @@
           (funcall (delay-closure x))
           (delay-forced x) )
       x ))
+
 #| Examples
+
+(delay (+ 1 1))
 
 (force (delay (+ 1 1)))
 
@@ -1343,6 +1449,423 @@ Examples |#
 (defmacro =apply (fn &rest args)
   `(apply ,fn *cont* ,@args) )
 
+#| Examples
+
+(macroexpand-1
+  '(=defun add1 (x) (=values (1+ x))) )
+
+(defun dft (tree)
+  (cond ((null tree) nil)
+        ((atom tree) (princ tree))
+        (t (dft (car tree))
+           (dft (cdr tree)) )))
+
+(setq *saved* nil)
+
+(=defun dft-node (tree)
+  (cond ((null tree) (restrt))
+        ((atom tree) (=values tree))
+        (t (push #'(lambda () (dft-node (cdr tree)))
+                 *saved* )
+           (dft-node (car tree)) )))
+
+(=defun restrt ()
+  (if *saved*
+    (funcall (pop *saved*))
+    (=values 'done) ))
+
+(=defun dft2 (tree)
+  (setq *saved* nil)
+  (=bind (node) (dft-node tree)
+    (cond ((eq node 'done) (=values nil))
+          (t (princ node)
+             (restrt) ))))
+
+(let ((t1 '(a (b (d c)) (c e (f i) g)))
+      (t2 '(1 (2 (3 8 7) 4 5))) )
+  (dft2 t1)
+  (dft2 t2) )
+
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Multiprocessing ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defstruct proc pri state wait)
+
+(proclaim '(special *procs* *proc*))
+
+(defvar *halt* (gensym))
+
+(defvar *default-proc*
+  (make-proc :state #'(lambda (x)
+                        (format t "~%>> ")
+                        (princ (eval (read)))
+                        (pick-process) )))
+
+(defmacro fork (expr pri)
+  `(prog1 ',expr
+     (push (make-proc
+             :state #'(lambda (,(gensym "FORK-"))
+                        ,expr
+                        (pick-process) )
+             :pri   ,pri )
+           *procs* )))
+
+(defmacro program (name args &body body)
+  `(=defun ,name ,args
+     (setq *procs* nil)
+     ,@body
+     (catch *halt* (loop (pick-process))) ))
+
+(defun pick-process ()
+  (multiple-value-bind (p val) (most-urgent-process)
+    (setq *proc* p
+          *procs* (delete p *procs*) )
+    (funcall (proc-state p) val) ))
+
+(defun most-urgent-process ()
+  (let ((proc1 *default-proc*) (max -1) (val1 t))
+    (dolist (p *procs*)
+      (let ((pri (proc-pri p)))
+        (if (> pri max)
+          (let ((val (or (not (proc-wait p))
+                         (funcall (proc-wait p)) )))
+            (when val
+              (setq proc1 p
+                    max pri
+                    val1 val ))))))
+    (values proc1 val1) ))
+
+(defun arbitrator (test cont)
+  (setf (proc-state *proc*) cont
+        (proc-wait *proc*) test )
+  (push *proc* *procs*)
+  (pick-process) )
+
+(defmacro wait (parm test &body body)
+  `(arbitrator #'(lambda () ,test)
+               #'(lambda (,parm) ,@body) ))
+
+(defmacro yield (&body body)
+  `(arbitrator nil #'(lambda (,(gensym "YIELD-")) ,@body)) )
+
+(defun setpri (n) (setf (proc-pri *proc*) n))
+
+(defun halt (&optional val) (throw *halt* val))
+
+(defun kill (&optional obj &rest args)
+  (if obj
+      (setq *procs* (apply #'delete obj *procs* args))
+      (pick-process) ))
+
+#| Examples
+
+(defvar *open-doors* nil)
+
+(=defun pedestrian ()
+  (wait d (car *open-doors*)
+        (format t "Entering ~A~%" d) ))
+
+(program ped ()
+  (fork (pedestrian) 1) )
+
+(ped)
+
+(defvar *bboard* nil)
+
+(defun claim (&rest f) (push f *bboard*))
+
+(defun unclaim (&rest f) (pull f *bboard* :test #'equal))
+
+(defun check (&rest f) (find f *bboard* :test #'equal))
+
+(=defun visitor (door)
+  (format t "Approach ~A. " door)
+  (claim 'knock door)
+  (wait d (check 'open door)
+    (format t "Enter ~A. " door)
+    (unclaim 'knock door)
+    (claim 'inside door) ))
+
+(=defun host (door)
+  (wait k (check 'knock door)
+    (format t "Open ~A. " door)
+    (claim 'open door)
+    (wait g (check 'inside door)
+      (format t "Close ~A.~%" door)
+      (unclaim 'open door) )))
+
+(program ballet ()
+  (fork (visitor 'door1) 1)
+  (fork (host 'door1) 1)
+  (fork (visitor 'door2) 1)
+  (fork (host 'door2) 1) )
+
+(ballet)
+
+(=defun capture (city)
+  (take city)
+  (setpri 1)
+  (yield
+    (fortify city) ))
+
+(=defun plunder (city)
+  (loot city)
+  (ransom city) )
+
+(defun take (c)    (format t "Liberating ~A.~%" c))
+(defun fortify (c) (format t "Rebuilding ~A.~%" c))
+(defun loot (c)    (format t "Nationalizing ~A.~%" c))
+(defun ransom (c)  (format t "Refinancing ~A.~%" c))
+
+(program barbarians ()
+  (fork (capture 'rome) 100)
+  (fork (plunder 'rome) 98) )
+
+(barbarians)
+
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Nondeterminism ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *paths* nil)
+(defconstant failsym '@)
+
+(defmacro choose (&rest choices)
+  (if choices
+      `(progn
+         ,@(mapcar #'(lambda (c)
+                       `(push #'(lambda () ,c) *paths*) )
+                   (reverse (cdr choices)) )
+         ,(car choices) )
+      '(fail) ))
+
+(defmacro choose-bind (var choices &body body)
+  `(cb #'(lambda (,var) ,@body) ,choices) )
+
+(defun cb (fn choices)
+  (if choices
+    (progn
+      (if (cdr choices)
+          (push #'(lambda () (cb fn (cdr choices)))
+                *paths* ))
+      (funcall fn (car choices)) )
+    (fail) ))
+
+(defun fail ()
+  (if *paths*
+      (funcall (pop *paths*))
+      failsym ))
+
+#| Examples
+
+(defun do2 (x)
+  (choose (+ x 2) (* x 2) (expt x 2)) )
+
+(do2 3)
+
+(fail)
+(fail)
+(fail)
+
+(choose-bind x '(marrakesh strambourg vegas)
+  (format nil "Let's go to ~A." x) )
+
+(fail)(fail)(fail)
+
+(=defun parlor-trick (sum)
+  (=bind (n1 n2) (two-numbers)
+    (if (= (+ n1 n2) sum)
+        `(the sum of ,n1 ,n2)
+        (fail) )))
+
+(=defun two-numbers ()
+  (choose-bind n1 '(0 1 2 3 4 5)
+    (choose-bind n2 '(0 1 2 3 4 5)
+      (=values n1 n2) )))
+
+(parlor-trick 5)
+
+|#
+
+;;;;;;;;;;;;;
+;;;; ATN ;;;;
+;;;;;;;;;;;;;
+
+(defmacro defnode (name &rest arcs)
+  `(=defun ,name (pos regs) (choose ,@arcs)) )
+
+(defmacro down (sub next &rest cmds)
+  `(=bind (* pos regs) (,sub pos (cons nil regs))
+     (,next pos ,(compile-cmds cmds)) ))
+
+(defmacro cat (cat next &rest cmds)
+  `(if (= (length *sent*) pos)
+       (fail)
+       (let ((* (nth pos *sent*)))
+         (if (member ',cat (types *))
+             (,next (1+ pos) ,(compile-cmds cmds))
+             (fail) ))))
+
+(defmacro jump (next &rest cmds)
+  `(,next pos ,(compile-cmds cmds)) )
+
+(defun compile-cmds (cmds)
+  (if (null cmds)
+      'regs
+      `(,@(car cmds) ,(compile-cmds (cdr cmds))) ))
+
+(defmacro up (expr)
+  `(let ((* (nth pos *sent*)))
+     (=values ,expr pos (cdr regs)) ))
+
+(defmacro getr (key &optional (regs 'regs))
+  `(let ((result (cdr (assoc ',key (car ,regs)))))
+     (if (cdr result) result (car result)) ))
+
+(defmacro set-register (key val regs)
+  `(cons (cons (cons ,key ,val) (car ,regs))
+         (cdr ,regs) ))
+
+(defmacro setr (key val regs)
+  `(set-register ',key (list ,val) ,regs) )
+
+(defmacro pushr (key val regs)
+  `(set-register ',key
+                 (cons ,val (cdr (assoc ',key (car ,regs))))
+                 ,regs ))
+
+(defmacro with-parses (node sent &body body)
+  (with-gensyms (pos regs "WITH-PARSES-")
+    `(progn
+       (setq *sent* ,sent)
+       (setq *paths* nil)
+       (=bind (parse ,pos ,regs) (,node 0 '(nil))
+         (if (= ,pos (length *sent*))
+             (progn ,@body (fail))
+             (fail) )))))
+
+#| Examples
+
+(compile-cmds '((setr a b) (setr c d)))
+
+(defun types (w)
+  (cdr (assoc w '((spot noun) (runs verb)))) )
+
+(with-parses s '(spot runs)
+  (format t "Parsing: ~A~%" parse) )
+
+(defun types (word)
+  (case word
+    ((do does did) '(aux v))
+    ((time times) '(n v))
+    ((fly flies) '(n v))
+    ((like) '(v prep))
+    ((liked likes) '(v))
+    ((a an the) '(det))
+    ((arrow arrows) '(n))
+    ((i you he she him her it) '(pron)) ))
+
+(defnode mods
+  (cat n mods/n
+    (setr mods *) ))
+
+(defnode mods/n
+  (cat n mods/n
+    (pushr mods *) )
+  (up `(n-group ,(getr mods))) )
+
+(defnode np
+  (cat det np/det
+    (setr det *) )
+  (jump np/det
+    (setr det nil) )
+  (cat pron pron
+    (setr n *) ))
+
+(defnode pron
+  (up `(np (pronoun ,(getr n)))) )
+
+(defnode np/det
+  (down mods np/mods
+    (setr mods *) )
+  (jump np/mods
+    (setr mods nil) ))
+
+(defnode np/mods
+  (cat n np/n
+    (setr n *) ))
+
+(defnode np/n
+  (up `(np (det ,(getr det))
+           (modifiers ,(getr nods))
+           (noun ,(getr n)) ))
+  (down pp np/pp
+    (setr pp *) ))
+
+(defnode np/pp
+  (up `(np (det ,(getr det))
+           (modifiers ,(getr mods))
+           (noun ,(getr n))
+           ,(getr pp) )))
+
+(defnode pp
+  (cat prep pp/prep
+    (setr prep *) ))
+
+(defnode pp/prep
+  (down np pp/np
+    (setr op *) ))
+
+(defnode pp/np
+  (up `(pp (prep ,(getr prep))
+           (obj ,(getr op)) )))
+
+(defnode s
+  (down np s/subj
+    (setr mood 'decl)
+    (setr subj *) )
+  (cat v v
+    (setr mood 'imp)
+    (setr subj '(np (pron you)))
+    (setr aux nil)
+    (setr v *) ))
+
+(defnode s/subj
+  (cat v v
+    (setr aux nil)
+    (setr v *) ))
+
+(defnode v
+  (up `(s (mood ,(getr mood))
+          (subj ,(getr subj))
+          (vcl (aux ,(getr aux))
+               (v ,(getr v)) )))
+  (down np s/obj
+    (setr obj *) ))
+
+(defnode s/obj
+  (up `(s (mood ,(getr mood))
+          (subj ,(getr subj))
+          (vcl (aux ,(getr aux))
+               (v ,(getr v)) )
+          (obj ,(getr obj)) )))
+
+
+(with-parses np '(a time fly like him)
+  (pprint parse) )
+
+(with-parses s '(time flies like an arrow)
+  (pprint parse) )
+
+1
+
+|#
+
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Query Compiler ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1368,7 +1891,7 @@ Examples |#
 #| An interpreter implementation
 
 (defmacro do-answers (query &body body)
-  (let ((binds (gensym "do-answers-")))
+  (let ((binds (gensym "DO-ANSWERS-")))
     `(dolist (,binds (interpret-query ',query))
        (let ,(mapcar #'(lambda (v)
                          `(,v (binding ',v ,binds)) )
